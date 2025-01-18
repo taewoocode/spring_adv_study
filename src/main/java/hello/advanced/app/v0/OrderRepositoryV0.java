3cb41b6de5278636507d6f204b8e4b6c8880af2f
taewoocode <qkrxodn6035@gmail.com>
2025-01-18 16:46:06 +0900

    초기 mvc패턴으로 V0 만들기

---
+++ src/main/java/hello/advanced/app/v0/OrderRepositoryV0.java
@@ -0,0 +1,28 @@
+package hello.advanced.app.v0;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+
+/**
+ * ComponentScan 대상
+ */
+@RequiredArgsConstructor
+@Repository
+public class OrderRepositoryV0 {
+
+    public void save(String itemId) {
+        if (itemId.equals("ex")) {
+            throw new IllegalStateException("예외 발생");
+        }
+        sleep(1000);
+    }
+
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
