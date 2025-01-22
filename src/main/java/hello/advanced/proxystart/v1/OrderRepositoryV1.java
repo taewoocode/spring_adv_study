e52ba604d8092e230e47e367ab97a37bc703049b
taewoocode <qkrxodn6035@gmail.com>
2025-01-22 22:45:43 +0900

    spring-proxy

---
+++ src/main/java/hello/advanced/proxystart/v1/OrderRepositoryV1.java
@@ -0,0 +1,6 @@
+package hello.advanced.proxystart.v1;
+
+public interface OrderRepositoryV1 {
+
+    void save(String itemId);
+}
---
+++ src/main/java/hello/advanced/proxystart/v1/OrderServiceV1.java
@@ -0,0 +1,5 @@
+package hello.advanced.proxystart.v1;
+
+public interface OrderServiceV1 {
+    void orderItem(String itemId);
+}
