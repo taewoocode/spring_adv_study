b9784b3f1adb960a4aa5c97a3a4bea29ba07b42b
taewoocode <qkrxodn6035@gmail.com>
2025-01-22 18:40:25 +0900

    콜백 테스트

---
+++ src/test/java/hello/advanced/trace/strategy/TemplateCallbackTest.java
@@ -0,0 +1,43 @@
+package hello.advanced.trace.strategy;
+
+import hello.advanced.trace.strategy.code.strategy.template.Callback;
+import hello.advanced.trace.template.TimeLogTemplate;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class TemplateCallbackTest {
+
+
+    /**
+     * 익명 내부 클래스
+     */
+    @Test
+    void callbackV1() {
+        TimeLogTemplate timeLogTemplate = new TimeLogTemplate();
+        timeLogTemplate.execute(new Callback() {
+            @Override
+            public void call() {
+                log.info("비즈니스 로직1 실행");
+            }
+        });
+
+        timeLogTemplate.execute(new Callback() {
+            @Override
+            public void call() {
+                log.info("비즈니스 로직2 실행");
+            }
+        });
+    }
+
+    /**
+     * lambda로 실행
+     */
+    @Test
+    void callbackV2() {
+        TimeLogTemplate timeLogTemplate = new TimeLogTemplate();
+        timeLogTemplate.execute(() -> log.info("비즈니스 로직1 실행"));
+
+        timeLogTemplate.execute(() -> log.info("비즈니스 로직2 실행"));
+    }
+}
