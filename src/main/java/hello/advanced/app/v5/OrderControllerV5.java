ed7415272e8ec03fd0a52dc4354f3cd54738bdac
taewoocode <qkrxodn6035@gmail.com>
2025-01-22 18:46:58 +0900

    콜백 테스트를 위한 v5 생성

---
+++ src/main/java/hello/advanced/app/v5/OrderControllerV5.java
@@ -0,0 +1,33 @@
+package hello.advanced.app.v5;
+
+import hello.advanced.trace.logtrace.LogTrace;
+import hello.advanced.trace.template.AbstractTemplate;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Controller + ResponseBody
+ */
+@RestController
+@RequiredArgsConstructor
+public class OrderControllerV5 {
+
+    /** 의존 관계 주입 **/
+    private final LogTrace trace;
+    private final OrderServiceV5 orderService ;
+
+
+    @GetMapping("/v5/request")
+    public String request(String itemId) {
+
+        AbstractTemplate<String> template = new AbstractTemplate<String>(trace) {
+            @Override
+            protected String call() {
+                orderService.orderItem(itemId);
+                return "ok";
+            }
+        };
+        return template.execute("OrderController.request()");
+    }
+}
